5addce792c380ab94689e2df499ef4d19d977882
susovan2003 <168700336+susovan2003@users.noreply.github.com>
2024-09-22 02:44:40 +0000

    variables js

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 14453
+let accountEmail = "susovansaha74@gmail.com"
+var accountPassword = "14421"
+accountCity = "Ranaghat" 
+
+// accountId = 211236//
+
+accountEmail= "swapna@google.com"
+accountPassword= "855632"
+accountCity= "Kolkata"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
