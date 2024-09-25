6a9e9627342456e9ea9fc73b655f051fb60997f4
susovan2003 <168700336+susovan2003@users.noreply.github.com>
2024-09-25 04:35:09 +0000

    String Methods Analysing

---
+++ 01_basics/stringfunctions.js
@@ -0,0 +1,30 @@
+// anchor method
+const name = "Hello World"
+// console.log(name.anchor("hello"));
+
+//Array.at()
+const array1 = [1,3,5,7];
+let index = 2
+//console.log(`An index of ${index} returns ${array1.at(index)}`);
+
+//String.prototype.blink()
+const contentString = "Hello, world";
+// console.log(contentString.blink());
+
+// String.prototype.bold()
+//console.log(contentString.bold());
+
+// String.prototype.charAt()
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+const indexnum = 3;
+//console.log(sentence.charAt(indexnum));
+
+//String.prototype.charCodeAt()
+const sentence2 = 'The quick brown fox jumps over the lazy dog.';
+
+const indexnum2 = 4;
+console.log(sentence2.charCodeAt(indexnum2));
+
+
+
+
